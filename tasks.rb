1b0d6e38db3cd1fcfef1a29760e2f9390ae361bb
Tradutor <31545223+nailsong@users.noreply.github.com>
2017-09-24 21:49:31 -0300

    Update tasks.rb

--- tasks.rb
+++ tasks.rb
@@ -1,42 +1,76 @@
-INSERI_TAREFA = 1
-VER_TAREFAS = 2
-SAIR = 0
+require_relative 'task'
 
-def menu()
-  puts 'Escolha uma tarefa'
-  puts "#{INSERI_TAREFA} - Inserir uma tarefa"
-  puts "#{VER_TAREFAS} - Ver todas tarefas"
-  puts '3 - Tarefa finalizada'
+INSERIR_TAREFA = 1
+VER_TAREFAS = 2
+BUSCAR_TAREFAS = 3
+MARCAR_COMO_FEITO = 4
+SAIR = 5
+OPCAO_INVALIDA = -1
+
+puts 'Bem-vindo a lista de tarefas'
+
+def menu
+  puts 'Escolha uma opção'
+  puts "#{INSERIR_TAREFA} - Inserir tarefa"
+  puts "#{VER_TAREFAS} - Ver Tarefas"
+  puts "#{BUSCAR_TAREFAS} - Buscar tarefas"
+  puts "#{MARCAR_COMO_FEITO} - Marcar uma tarefa como feita"
   puts "#{SAIR} - Sair"
+  gets.to_i
 end
 
-  tarefas = []
+def imprimir_tarefas(tarefas_para_imprimir)
+  tarefas_para_imprimir.each do |t|
+    puts t
+  end
+end
 
-  puts 'Bem-vindo ao Task List'
+ opcao = OPCAO_INVALIDA
+ tarefas = []
+ tarefas_concluidas = []
 
-menu
+while opcao != SAIR do
 
-  opcao = gets.to_i
+  if opcao == INSERIR_TAREFA
+    puts 'Informe o nome da tarefa'
+    nome_tarefa = gets.chomp
 
-  puts "Opção Escolhida: #{opcao}"
+    puts 'Tarefa já realizada? Sim: 1 -- Não: 2'
+    status_tarefa = gets.to_i
 
-  while(opcao != SAIR) do
-
-    if(opcao == INSERI_TAREFA)
-      puts 'Informe o nome da tafera'
-      nome_tarefa = gets.chomp
-      tarefas << "Tarefa: #{nome_tarefa}"
-      puts 'Nova tarefa cadastrada com sucesso'
-
-    elsif(opcao == VER_TAREFAS)
-      puts tarefas
-    elsif(opcao == 3)
-      puts 'Tarefas finalizadas'
+    if status_tarefa == 1
+      tarefa = Task.new(nome_tarefa, true)
+    elsif
+      tarefa = Task.new(nome_tarefa)
     end
 
-    menu
+    tarefa = Task.new(nome_tarefa)
 
-    opcao = gets.to_i
+    tarefas << tarefa
+
+    puts "Nova tarefas cadastrada: #{nome_tarefa}"
+
+  elsif opcao == VER_TAREFAS
+    puts 'Ver todas tarefas'
+
+    imprimir_tarefas(tarefas)
+
+  elsif opcao == BUSCAR_TAREFAS
+    puts 'Buscar tarefas'
+
+    puts 'Informe um texto para buscar:'
+    texto_busca = gets.chomp.downcase
+
+    tarefas_encontradas = tarefas.select do |t|
+      t.include_text? texto_busca
+    end
+
+    imprimir_tarefas(tarefas_encontradas)
+
   end
+
+opcao = menu
+
+end
 
 puts 'Até logo'
